b2f1c3926e0d3b6bfabbdcda486c610baf423406
James Perkins <james.r.perkins@tuta.io>
2019-04-10 20:10:27 -0400

    dev-dependacy mistake

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import x from './';
 
-test("hexcolor", t => {
+test("SHA256", t => {
 	t.true(x.test('0c67b2c1fcfba9fb71ff6feae41abd61d591df8ecabebefdba51db4d3cf9f1cf'));
     t.false(x.test('0c67b2c1fcfba9fb71ff6feae41abd61d591df8ecabebefdba51db4d3ct9f1cf'));
 
